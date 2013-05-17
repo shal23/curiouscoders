c6c256eb57c336432cb981e1abe0734681a88ab9
David Evans <davidjsevans@gmail.com>
2013-05-17 11:59:18 +1000

    update nav and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Curiouscoders::Application.routes.draw do
-  #resources :tutorials
-
 
   authenticated :user do
-  root :to => 'home#index'
-  
+	  root :to => 'home#index'
   end
 
   root :to => "home#index"
+  
   devise_for :users
   resources :users 
   resources :tutorials 
