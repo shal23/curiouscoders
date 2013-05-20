57328c852c5f74ea5a8bcfeb1f8c5fb72c764214
Bazay <baronbloomer@gmail.com>
2013-05-20 12:41:00 +1000

    Added autocomplete to compose message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518054941) do
+ActiveRecord::Schema.define(:version => 20130520010225) do
 
   create_table "conversations", :force => true do |t|
     t.integer  "team_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130518054941) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.integer  "user_id"
+    t.string   "recipient_name"
   end
 
   create_table "posts", :force => true do |t|
